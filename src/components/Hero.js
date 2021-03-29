08a8a58d5164102fa453f73b5af11b011e5faaa1
Jhunsen Samonte <67861231+jhunxein@users.noreply.github.com>
2021-03-29 08:42:10 -0800

    Changing h1 to h2

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,9 +5,9 @@ function Hero() {
 	return (
 		<article className="hero">
 			<div className="text-hero-wrapper">
-				<h1>
+				<h2>
 					we're<span>coming</span> <span>soon</span>
-				</h1>
+				</h2>
 				<p>
 					Hello fellow shoppers! We're currently building our new fashion store. Add
 					your email below to stay up-to-date with announcements and our launch
